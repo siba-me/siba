b9c8783daf55756f1f5d9984b20be220267c3332
siba-me <siba.azab268@gmail.com>
2018-09-21 09:50:15 +0300

    "option menu"

--- app/src/main/java/com/example/user/siba/MainActivity.java
+++ app/src/main/java/com/example/user/siba/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.user.siba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main2);
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.item1:
+                    Toast.makeText(this, "item menu 1 ", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            switch (item.getItemId()) {
+                case R.id.item2:
+                    Toast.makeText(this, "item menu 2 ", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            switch (item.getItemId()) {
+                case R.id.item3:
+                    Toast.makeText(this, "item menu 3 ", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
+
+        }
     }
-}
