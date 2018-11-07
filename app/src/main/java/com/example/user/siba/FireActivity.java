fad4276012d6e7d801f8aed82ebd470c4a6d847d
siba-me <siba.azab268@gmail.com>
2018-11-07 14:17:30 +0200

    "dataBase - FireActivity"

--- app/src/main/java/com/example/user/siba/FireActivity.java
+++ app/src/main/java/com/example/user/siba/FireActivity.java
@@ -2,6 +2,7 @@ package com.example.user.siba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,8 +21,9 @@ import org.w3c.dom.Text;
 
 import java.sql.Struct;
 import java.util.ArrayList;
+import java.util.Map;
 
-public class FireActivity extends AppCompatActivity implements View.OnClickListener {
+public class FireActivity extends AppCompatActivity  {
     EditText etEmail,etPass;
     TextView tvEmail,tvPro;
     Button btSave;
@@ -32,7 +35,7 @@ public class FireActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire);
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference().child("Name");
+        final DatabaseReference myRef = database.getReference("Users");
 
         etEmail = (EditText) findViewById(R.id.etEmail);
         etPass = (EditText) findViewById(R.id.etPass);
@@ -40,29 +43,73 @@ public class FireActivity extends AppCompatActivity implements View.OnClickListe
         tvPro = (TextView) findViewById(R.id.tvPro);
         btSave = (Button) findViewById(R.id.btSave);
         lvUsers = (ListView) findViewById(R.id.lvUsers);
-        users = new ArrayList<String>();
-        ArrayAdapter adapter;
+        users = new ArrayList<>();
+        final ArrayAdapter adapter;
         adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,users);
         lvUsers.setAdapter(adapter);
-
-        myRef.addValueEventListener(new ValueEventListener() {
+        btSave.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                tvEmail.setText(value);
+            public void onClick(View v) {
+                String email = etEmail.getText().toString();
+                myRef.child("Name").setValue(email);
+                myRef.child("Profession").setValue("student");
+       //         myRef.child("Name").push().setValue(email);
+            }
+        });
+
+
+    //listener
+//        myRef.child("users").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Map<String,String> map = (Map<String, String>) dataSnapshot.getValue();
+//                Log.v("E_VALUE","Data"+ dataSnapshot.getValue());
+//                String name = map.get("name");
+//                String profession = map.get("profession");
+//                tvEmail.setText(name);
+//                tvPro.setText(profession);
+//                //String value = dataSnapshot.getValue(String.class);
+//                // tvEmail.setText(value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//
+//            }
+//        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String name = dataSnapshot.getValue(String.class);
+                users.add(name);
+                adapter.notifyDataSetChanged();
+                //refresh to the arrayList
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
-
             }
         });
-
-    }
-
-    @Override
-    public void onClick(View v) {
 
     }
 }
